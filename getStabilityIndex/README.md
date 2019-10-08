3d02b6ffe6b3787fc4d2b569089ca407ee2820b0
Aleksei Evstafev <aleksey.evstafiev@main.russianpost.ru>
2019-10-08 17:15:31 +0300

    Version 1.0

---
+++ getStabilityIndex/README.md
@@ -0,0 +1,2 @@
+# getStabilityIndex
+The script collects SystemStabilityIndex metrics and calculte average value. Is used as Zabbix UserParameter for detect and troubleshoot applications crushes.
---
+++ getStabilityIndex/getStabilityIndex.ps1
@@ -0,0 +1,17 @@
+﻿#
+# The script collects SystemStabilityIndex metrics and calculte average value.
+#
+
+$stIndex = @() 
+
+$count = 0
+
+gwmi Win32_ReliabilityStabilityMetrics | Select -exp SystemStabilityIndex | % {$stIndex += $_}
+
+foreach ($index in $stIndex) {
+    $count += $index
+}
+
+$count = [math]::Round($count/$stIndex.count, 4)
+
+return $count
